116a694d6bf8912027834407a9db725462c718e0
Kaik Selhorst <contato.kaikselhorst@gmail.com>
2022-05-23 13:28:51 -0400

    refactor: Modification in variable names Simplifying.

--- script/SmoothScroll.js
+++ script/SmoothScroll.js
@@ -20,24 +20,28 @@ export class SmoothScroll {
 }
 
 export class activeSmoothScrollEfect{
-  constructor(elementIndicatorSection,section){
-    this.elementIndicatorSection = document.querySelectorAll(elementIndicatorSection)
-    this.section = document.querySelectorAll(section)
-    this.sectionIndicator = document.querySelector('.section_indicator')
+  constructor(section){
+    this.section = Array.from(document.querySelectorAll(section))
     this.handleScroll = this.handleScroll.bind(this)
     this.activeNavScroll()
+    this.addSpanItem()
     this.handleScroll()
-  }
-  handleScroll(){
-    this.section.forEach((itemSection,i) => {
-      if(window.pageYOffset > itemSection.offsetTop - window.innerHeight * 0.5){
-        this.elementIndicatorSection[i].classList.add('active')
-      }else{
-        this.elementIndicatorSection[i].classList.remove('active')
-      }
-    })
   }
   activeNavScroll(){
     window.addEventListener('scroll',this.handleScroll)
+  }
+  addSpanItem(){
+    let indicator = document.querySelector('.section_indicator')
+    indicator.innerHTML = this.section.map(item => `<span></span>`).join('')
+  }
+  handleScroll(){
+    let spanItems = document.querySelectorAll('.section_indicator > span')
+    this.section.forEach((itemSection,i) => {
+      if(window.pageYOffset > itemSection.offsetTop - window.innerHeight * 0.5){
+        spanItems[i].classList.add('active')
+      }else{
+        spanItems[i].classList.remove('active')
+      }
+    })
   }
 }
