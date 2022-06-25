7bc9ca2f652f532e145769ae1180f432b3494cde
Kaik Selhorst <Contato.KaikSelhorst@gmail.com>
2022-06-24 21:22:37 -0400

    Fix: optimizing and organizing styles

--- script/index.js
+++ script/index.js
@@ -1,11 +1,10 @@
-import {SwitchTheme} from "./switchTheme.js"
-import {MenuMobile} from "./menuMobile.js"
-import {GitStatus} from "./footergit.js"
-import {SmoothScroll,activeSmoothScrollEfect} from "./SmoothScroll.js"
+import { SwitchTheme } from "./switchTheme.js";
+import { MenuMobile } from "./menuMobile.js";
+import { GitStatus } from "./footergit.js";
+import { SmoothScroll, activeSmoothScrollEfect } from "./SmoothScroll.js";
 
-
-new SwitchTheme('.btn-theme','#popup')
-new MenuMobile(".btn-mobile",'.navigation')
-new GitStatus('#git_stars',"#git_fork",'KaikSelhorstPixel')
-new SmoothScroll('a[href^="#"]')
-new activeSmoothScrollEfect('.content > section')
+new SwitchTheme(".btn-theme", "#popup");
+new MenuMobile(".btn-mobile", ".navigation");
+new GitStatus("#git_stars", "#git_fork", "KaikSelhorstPixel");
+new SmoothScroll('a[href^="#"]');
+new activeSmoothScrollEfect(".content > section");
