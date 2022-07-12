7feae0eb182d4d4d70b9fd083bb29de1d466b9c3
Kaik Selhorst <Contato.KaikSelhorst@gmail.com>
2022-07-12 19:07:32 -0400

    Fix: I modified the sidebar so that it can serve the use

--- script/SmoothScroll.js
+++ script/SmoothScroll.js
@@ -1,47 +1,66 @@
-import {smoothScrollTo} from './SmoothEffect.js'
+import { smoothScrollTo } from "./SmoothEffect.js";
 
 export class SmoothScroll {
-  constructor(menuItems){
-    this.menuItems = document.querySelectorAll(menuItems)
-    this.eventaddInItems()
+  constructor(menuItems) {
+    this.menuItems = document.querySelectorAll(menuItems);
+    this.eventaddInItems();
   }
-  eventaddInItems(){
-    this.menuItems.forEach(element => {
-      element.addEventListener('click',this.ScrollToIdClick )
-    })
+  eventaddInItems() {
+    this.menuItems.forEach((element) => {
+      element.addEventListener("click", this.ScrollToIdClick);
+    });
   }
-  ScrollToIdClick(event){
-    event.preventDefault()
-    const element = event.target
-    const id = element.getAttribute('href')
-    const to = document.querySelector(id).offsetTop - 80
-    smoothScrollTo(0,to)   
+  ScrollToIdClick(event) {
+    event.preventDefault();
+    const element = event.target;
+    const id = element.getAttribute("href");
+    const to = document.querySelector(id).offsetTop - 80;
+    smoothScrollTo(0, to);
   }
 }
 
-export class activeSmoothScrollEfect{
-  constructor(section){
-    this.section = Array.from(document.querySelectorAll(section))
-    this.handleScroll = this.handleScroll.bind(this)
-    this.activeNavScroll()
-    this.addSpanItem()
-    this.handleScroll()
+export class activeSmoothScrollEfect {
+  constructor(section) {
+    this.section = Array.from(document.querySelectorAll(section));
+    this.handleScroll = this.handleScroll.bind(this);
+    this.activeNavScroll();
+    this.addIndicator();
+    this.eventaddInItemsIndicator();
+    this.handleScroll();
   }
-  activeNavScroll(){
-    window.addEventListener('scroll',this.handleScroll)
+  activeNavScroll() {
+    window.addEventListener("scroll", this.handleScroll);
   }
-  addSpanItem(){
-    let indicator = document.querySelector('.section_indicator')
-    indicator.innerHTML = this.section.map(item => `<span></span>`).join('')
+  addIndicator() {
+    let indicator = document.querySelector(".section_indicator");
+    indicator.innerHTML = this.section
+      .map((item) => `<a href="#${item.id}"></a>`)
+      .join("");
   }
-  handleScroll(){
-    let spanItems = document.querySelectorAll('.section_indicator > span')
-    this.section.forEach((itemSection,i) => {
-      if(window.pageYOffset > itemSection.offsetTop - window.innerHeight * 0.5){
-        spanItems[i].classList.add('active')
-      }else{
-        spanItems[i].classList.remove('active')
+  eventAddInItemsIndicator() {
+    let indicatorItems = document.querySelectorAll(".section_indicator a");
+    console.log(indicatorItems);
+    indicatorItems.forEach((element) => {
+      element.addEventListener("click", (event) => {
+        event.preventDefault();
+        const element = event.target;
+        const id = element.getAttribute("href");
+        const to = document.querySelector(id).offsetTop - 80;
+        smoothScrollTo(0, to);
+      });
+    });
+  }
+  handleScroll() {
+    let indicatorItems = document.querySelectorAll(".section_indicator > a");
+    this.section.forEach((itemSection, i) => {
+      if (
+        window.pageYOffset >
+        itemSection.offsetTop - window.innerHeight * 0.5
+      ) {
+        indicatorItems[i].classList.add("activeSection");
+      } else {
+        indicatorItems[i].classList.remove("activeSection");
       }
-    })
+    });
   }
 }
